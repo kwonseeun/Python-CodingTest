4922d44147035e24818b9c95128d54c7974b0001
kwonseeun <0115km@naver.com>
2022-06-03 13:57:50 +0900

    수정,삭제 추가

--- ex03.py
+++ ex03.py
@@ -5,6 +5,40 @@ while True:
         name = input ('이름 입력:').title()
         phone = input('전화번호 입력:') 
         members[name] = phone
-        print('------------')
+        print('-------------------')
         print(f'*****{name} 입력 완료*****')
-        print('------------')
+        print('-------------------')
+        
+    elif menu =='f':
+        name = input('검색할 이름 입력:').title()
+        phone = members.get(name, '존재하지 않습니다.')
+        print('--------------------')
+        print(f'{name}:', phone)
+        print('--------------------')
+         
+    elif menu=='u':
+        name = input ('수정할 이름 입력:').title()
+        if name not in members.keys():    
+            print('-----------------')
+            print(f'{name} 회원은 존재하지 않습니다.')
+            print('-----------------')
+
+    elif menu=='d':
+        name = input ('삭제할 이름 입력:').title()
+        if name not in members.keys():
+            print('------------------')
+            print(f'{name} 회원은 존재하지 않습니다.')
+            print('------------------')
+            continue
+    else:
+        ask = input(f"{name} 회원을 정말로 삭제할까요?(y)").lower()
+        if ask == y :
+            del member[name]
+            print('-----------------')
+            print(f"******{name} 삭제 완료*******")
+            print('-----------------')
+        else:
+            print('-----------------')
+            print(f'{name} 회원을 삭제하지 않았습니다.')
+            print('-----------------')
+ 
